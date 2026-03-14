e3b5d91d0d60fe29841fd53bbf826fd822d80cff
dania abdullah <149895924+engdania@users.noreply.github.com>
2026-03-15 02:45:58 +0300

    Delete auth.ts

--- auth.ts
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-export const useStore = create((set) => ({
-  profile: null,
-  isLoggedIn: false,
-  setProfile: (profile: any) => set({ profile }),
-  setIsLoggedIn: (isLoggedIn: boolean) => set({ isLoggedIn }),
-}));
